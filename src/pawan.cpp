195c86ec29770d35a66f11f4104edf8830efde36
Puneet Singh <16326029+punsingh@users.noreply.github.com>
2021-04-02 22:32:44 -0700

    New creating and reading binary file functions along with simple vortex ring example

--- src/pawan.cpp
+++ src/pawan.cpp
@@ -17,9 +17,7 @@ int main(int argc, char* argv[]){
 	std::cout << std::setprecision(16) << std::scientific;
 	std::cout << "PArticle Wake ANalysis" << std::endl;
 	pawan::__io *IO = new pawan::__io();
-	IO->print();
-	OUT(IO->getFile());
-	pawan::__wake *W = new pawan::__wake(IO);
+	pawan::__wake *W = new pawan::__wake(1.0,1.0,1.0,16);
 	W->print();
 	delete W;
 	delete IO;
